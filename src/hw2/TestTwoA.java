14f5ebedfe9982bdd2c045c355b54929f06fc791
doughert1 <doughert1@grinnell.edu>
2014-01-24 15:47:57 -0600

    added test file

---
+++ src/hw2/TestTwoA.java
@@ -0,0 +1,17 @@
+package hw2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTwoA
+{
+
+  @Test
+  public void
+    test ()
+  {
+    fail ("Not yet implemented");
+  }
+
+}
--- src/hw2/TwoA.java
+++ src/hw2/TwoA.java
@@ -10,9 +10,9 @@ public class TwoA
     if ((int) i * b == a)
       {
         multInt = true;
-      }
+      }// if
     return multInt;
-  }
+  }// isMultiple
 
   public static boolean
     isOdd (int i)
@@ -22,9 +22,9 @@ public class TwoA
     if (test == -1)
       {
         odd = true;
-      }
+      }// if
     return odd;
-  }
+  }// isOdd
 
   public static int
     oddSumTo (int n)
@@ -33,9 +33,9 @@ public class TwoA
     for (int j = 1; j < n; j += 2)
       {
         sum += j;
-      }
+      }// for
     return sum;
-  }
+  }// oddSumTo
 
   public static boolean
     isOddProd (int[] ints)
@@ -48,9 +48,40 @@ public class TwoA
             if (isOdd (ints[l] * ints[m]))
               {
                 oddCount = true;
-              }
-          }
-      }
+              }// if
+          }// for(m)
+      }// for(l)
     return oddCount;
-  }
-}
+  }// isOddProd
+
+  public static boolean
+    allDistinct (int[] vals)
+  {
+    boolean distinct = true;
+    for (int n = 0; n < vals.length; n++)
+      {
+        for (int o = 1; o < vals.length; o++)
+          {
+            if (vals[n] == vals[o])
+              {
+                distinct = false;
+              }// if
+          }// for(o)
+      }// for(n)
+    return distinct;
+  }// allDistinct
+
+  public static void
+  reverseInts (int[] nums)
+  {
+  int temp;
+  int len = nums.length - 1;
+  for(int p = 0; p < len; p++)
+    {
+      temp = nums[p];
+      nums[p] = nums[len];
+      nums[len] = temp;
+      len--;
+    }//for(p)
+  }//reverseInts
+}// TwoA
