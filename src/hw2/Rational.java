af8836d34f5347502e2e4e0a37337081debd38df
doughert1 <doughert1@grinnell.edu>
2014-01-27 20:40:59 -0600

    TwoC renamed Rational; problem worked on

---
+++ src/hw2/Rational.java
@@ -0,0 +1,79 @@
+package hw2;
+
+public class Rational
+{
+  
+  /**   
+   * Get the numerator of this rational number. (Observer)
+   */
+  public int
+    numerator ()
+  {
+    return 0; // STUB
+  } // numerator
+
+  /**
+   * Get the denominator of a rational number. (Observer)
+   */
+  public int
+    denominator ()
+  {
+    return 0; // STUB
+  } // denominator
+
+  /**
+   * Multiply this rational number by an integer. 
+   * (Observer/Constructor)
+   */
+  public Rational
+    multiply (int multiplicand)
+  {
+    return null; // STUB
+  } // multiply (int)
+
+  /**
+   * Divide a rational number by an integer (Observer/Constructor)
+   */
+  public Rational
+    divide (int divisor)
+  {
+    return null; // STUB
+  } // divide (int)
+
+  /**
+   * Flip a rational number (denominator/numerator) (Mutator)
+   */
+  public void
+    inverse ()
+  {
+    //STUB
+  } // inverse
+
+  /**
+   * Return the greatest common divisor of two rational numbers 
+   * (Observer)
+   */
+  public int
+    gcd (Rational a, Rational b)
+  {
+    return 0; // STUB
+  }// gcd(Rational)
+
+  /**
+   * Return the least common denominator of a rational number. 
+   * (Observer)
+   */
+  public int
+    lcd ()
+  {
+    return 0; // STUB
+  }// lcd
+  
+  /**
+   * Simplify a rational number (if possible) (Mutator)
+   */
+  public void simplify()
+  {
+    //STUB
+  }//simplify
+}//Rational
