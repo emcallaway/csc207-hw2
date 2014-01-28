af0e2445bc1ef71a0491a99f038cf928d8834a6a
doughert1 <doughert1@grinnell.edu>
2014-01-27 19:43:33 -0600

    Still working on TwoC...

--- src/hw2/TwoC.java
+++ src/hw2/TwoC.java
@@ -1,40 +1,76 @@
 package hw2;
 
+public class TwoC
+{
+  
+  public Rational(int num, int den)
+  {
+    
+  }
+  
+  /**
+   * Get the numerator of this rational number. (Observer)
+   */
+  public int
+    numerator ()
+  {
+    return 0; // STUB
+  } // numerator
 
-public class TwoC {
-	/**
-	 * Get the numerator of this rational number. (Observer)
-	 */
-	public int numerator() {
-		return 0; // STUB
-	} // numerator
-	
-	/**
-	 *Get the denominator of a rational number. (Observer)
-	*/
-	public int denominator() {
-		return 0; // STUB
-	} //denominator
+  /**
+   * Get the denominator of a rational number. (Observer)
+   */
+  public int
+    denominator ()
+  {
+    return 0; // STUB
+  } // denominator
 
-	/**
-	 * Multiply this rational number by an integer. (Observer/Constructor)
-	 */
-	public Rational multiply(int multiplicand) {
-		return null; // STUB
-	} // multiply (int)
-	
-	/**
-	 * Divide a rational number by an integer (Observer/Constructor)
-	*/
-	public Rational divide(int divisor) {
-		return null; // STUB
-	} // divide (int)
-	
-	/**
-	 * Flip a rational number (denominator/numerator) (Mutator)
-	*/ 
-	public Rational inverse(int rational) {
-		return null; // STUB
-	} // multiply (int)
+  /**
+   * Multiply this rational number by an integer. 
+   * (Observer/Constructor)
+   */
+  public Rational
+    multiply (int multiplicand)
+  {
+    return null; // STUB
+  } // multiply (int)
 
+  /**
+   * Divide a rational number by an integer (Observer/Constructor)
+   */
+  public Rational
+    divide (int divisor)
+  {
+    return null; // STUB
+  } // divide (int)
+
+  /**
+   * Flip a rational number (denominator/numerator) (Mutator)
+   */
+  public void
+    inverse ()
+  {
+    //STUB
+  } // inverse
+
+  /**
+   * Return the greatest common devisor of two rational numbers 
+   * (Observer)
+   */
+  public int
+    gcd (Rational a, Rational b)
+  {
+    return 0; // STUB
+  }// gcd(Rational)
+
+  /**
+   * Return the least common denominator of a rational number. 
+   * (Observer)
+   */
+  public int
+    lcd ()
+  {
+    return 0; // STUB
+  }// lcd
 }
